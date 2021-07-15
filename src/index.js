a0e5fa9cfaebc4d8de1bec0761f3db86878a6986
gerasaka <gerryjulioo@gmail.com>
2021-07-15 07:33:00 +0700

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -4,20 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const imgUrl = "https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif";
-
-const searchForm = (
-  <form>
-    <input type="text"/>
-    <input type="submit" value="Search" />
-  </form>
-);
-
-const img = <img src={imgUrl}></img>
-
 ReactDOM.render(
-  searchForm,
-  img,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
